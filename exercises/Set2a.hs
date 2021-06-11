131e761787a0b4c7eb0b80d7c2524e69758a948e
Ville Peurala <ville.peurala@op.fi>
2021-06-11 17:28:51 +0300

    8/11/2

--- exercises/Set2a.hs
+++ exercises/Set2a.hs
@@ -119,7 +119,8 @@ safeDiv x y = if y == 0 then Nothing else Just (x `div` y)
 --   greet "John" (Just "Smith")  ==> "Hello, John Smith!"
 
 greet :: String -> Maybe String -> String
-greet first last = todo
+greet first Nothing = "Hello, " ++ first ++ "!"
+greet first (Just last) = "Hello, " ++ first ++ " " ++ last ++ "!"
 
 ------------------------------------------------------------------------------
 -- Ex 9: safe list indexing. Define a function safeIndex so that
