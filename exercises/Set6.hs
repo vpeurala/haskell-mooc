5dac590e1ebc70d36fc29699fc3a132d00cd8499
Ville Peurala <ville.peurala@op.fi>
2021-06-14 11:33:30 +0300

    2 / 12 / 6

--- exercises/Set6.hs
+++ exercises/Set6.hs
@@ -25,10 +25,14 @@ instance Eq Country where
 -- Remember minimal complete definitions!
 
 instance Ord Country where
-  compare = todo -- implement me?
-  (<=) = todo -- and me?
-  min = todo -- and me?
-  max = todo -- and me?
+  compare Finland Finland = EQ
+  compare Norway Norway = EQ
+  compare Switzerland Switzerland = EQ
+  compare Finland Norway = LT
+  compare Finland Switzerland = LT
+  compare Norway Switzerland = LT
+  compare Norway Finland = GT
+  compare Switzerland _ = GT
 
 ------------------------------------------------------------------------------
 -- Ex 3: Implement an Eq instance for the type Name which contains a String.
