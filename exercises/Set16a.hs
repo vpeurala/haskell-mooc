730e48b4f5bfc8e5b18969033ca26ceb88a8484b
Ville Peurala <ville.peurala@op.fi>
2021-07-02 20:16:45 +0300

    2 / 7 / 16a

--- exercises/Set16a.hs
+++ exercises/Set16a.hs
@@ -50,7 +50,7 @@ isSorted xs = sort xs === xs
 --  +++ OK, passed 1 test.
 
 sumIsLength :: Show a => [a] -> [(a,Int)] -> Property
-sumIsLength input output = todo
+sumIsLength input output = length input === sum (map snd output)
 
 -- This is a function that passes the sumIsLength test but is wrong
 freq1 :: Eq a => [a] -> [(a,Int)]
