91db535692ba1b465acdbbb4ec4885316149f894
Ville Peurala <ville.peurala@op.fi>
2021-07-02 15:08:45 +0300

    1 / 11 / 12

--- exercises/Set12.hs
+++ exercises/Set12.hs
@@ -17,7 +17,7 @@ import Mooc.Todo
 --   incrementAll (Just 3.0)  ==>  Just 4.0
 
 incrementAll :: (Functor f, Num n) => f n -> f n
-incrementAll x = todo
+incrementAll x = fmap (+1) x
 
 ------------------------------------------------------------------------------
 -- Ex 2: Sometimes one wants to fmap multiple levels deep. Implement
