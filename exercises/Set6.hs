929b0af6615bca255c52a0cc7a1005f04e38044d
Ville Peurala <ville.peurala@op.fi>
2021-07-06 20:35:41 +0300

    12 / 12 / 6

--- exercises/Set6.hs
+++ exercises/Set6.hs
@@ -263,3 +263,20 @@ data Color = Red | Green | Blue
 data Suit = Club | Spade | Diamond | Heart
   deriving (Show, Eq)
 
+class Cycle a where
+  step :: a -> a
+  stepMany :: Int -> a -> a
+  stepMany 0 a = a
+  stepMany 1 a = step a
+  stepMany n a = stepMany (n - 1) (step a)
+
+instance Cycle Color where
+  step Red = Green
+  step Green = Blue
+  step Blue = Red
+
+instance Cycle Suit where
+  step Club = Spade
+  step Spade = Diamond
+  step Diamond = Heart
+  step Heart = Club
