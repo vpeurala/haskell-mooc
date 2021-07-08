4715355c468111e65f3d519edaec86d8586d0998
Ville Peurala <ville.peurala@op.fi>
2021-07-08 13:37:08 +0300

    8 / 10 / 8

--- exercises/Set8.hs
+++ exercises/Set8.hs
@@ -431,8 +431,9 @@ instance Transform Flip where
 data Chain a b = Chain a b
   deriving Show
 
-instance Transform (Chain a b) where
-  apply = todo
+instance (Transform a, Transform b) => Transform (Chain a b) where
+  apply (Chain a b) pic = apply a $ apply b pic
+
 ------------------------------------------------------------------------------
 
 -- Now we can redefine largeVerticalStripes using the above Transforms.
