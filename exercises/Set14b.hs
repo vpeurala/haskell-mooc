381b1b9a3180eb896c143e24409ec030790a204f
Ville Peurala <ville.peurala@op.fi>
2021-07-13 15:42:05 +0300

    5 / 8 / 14b

--- exercises/Set14b.hs
+++ exercises/Set14b.hs
@@ -212,7 +212,7 @@ encodeResponse t = LB.fromStrict (encodeUtf8 t)
 -- Remember:
 -- type Application = Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
 simpleServer :: Application
-simpleServer request respond = todo
+simpleServer _ respond = respond (responseLBS status200 [] "BANK")
 
 ------------------------------------------------------------------------------
 -- Ex 6: Now we finally have all the pieces we need to actually
